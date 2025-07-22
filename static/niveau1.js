7ffee35422761fc96e60554953f2c7cbf86de7e8
Paul Millaret <paulmillaret@gmail.com>
2025-07-22 14:06:04 +0200

    Niveau 1: Boutons vert/rouge et son faux intégrés

--- static/niveau1.js
+++ static/niveau1.js
@@ -1,28 +1,25 @@
 // Sons
 const sonBon = new Audio("static/sons/Son_correct.mp3");
-const sonMauvais = new Audio("sattic/sons/Son_faux.mp3");
+const sonMauvais = new Audio("static/sons/Son_faux.mp3"); // Chemin corrigé ici : "static" au lieu de "sattic"
 
 // Tableau des questions pour le Niveau 1
-// Le user devra adapter les chemins des images et les reponses
 const questionsNiveau1 = [
-    { src: "static/image_nv_1/Image1.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "non" }, 
-    { src: "static/image_nv_1/Image2.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "oui" }, 
+    { src: "static/image_nv_1/Image1.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "non" },
+    { src: "static/image_nv_1/Image2.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "oui" },
     { src: "static/image_nv_1/Image3.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "non" },
-    { src: "static/image_nv_1/Image4.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "oui" }, 
-    { src: "static/image_nv_1/Image5.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "non" }, 
-    { src: "static/image_nv_1/Image6.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "oui" }, 
-    { src: "static/image_nv_1/Image7.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "non" }, 
-    { src: "static/image_nv_1/Image8.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "non" }, 
+    { src: "static/image_nv_1/Image4.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "oui" },
+    { src: "static/image_nv_1/Image5.jpg", question: "Ce grain de beaute est-il : SYMETRIQUE ?", reponse: "non" },
+    { src: "static/image_nv_1/Image6.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "oui" },
+    { src: "static/image_nv_1/Image7.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "non" },
+    { src: "static/image_nv_1/Image8.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "non" },
     { src: "static/image_nv_1/Image9.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "non" },
-    { src: "static/image_nv_1/Image10.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "oui" }, 
-    { src: "static/image_nv_1/Image11.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "non" }, 
+    { src: "static/image_nv_1/Image10.jpg", question: "Ce grain de beaute a t'il : des BORDS REGULIERS ?", reponse: "oui" },
+    { src: "static/image_nv_1/Image11.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "non" },
     { src: "static/image_nv_1/Image12.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "non" },
     { src: "static/image_nv_1/Image13.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "non" },
     { src: "static/image_nv_1/Image14.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "oui" },
     { src: "static/image_nv_1/Image15.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "non" },
     { src: "static/image_nv_1/Image16.jpg", question: "Ce grain de beaute est-il : MONOCHROME ?", reponse: "non" },
-
-
 ];
 
 let index = 0;
@@ -30,6 +27,7 @@ let score = 0;
 
 const zoneJeu = document.getElementById("jeu");
 
+// Contenu HTML du niveau 1, avec les IDs de boutons mis à jour
 zoneJeu.innerHTML = `
     <h1>Niveau 1 : Savoir Decrire</h1>
 
@@ -45,17 +43,16 @@ zoneJeu.innerHTML = `
     <h2 id="question-texte"></h2>
 
     <div class="buttons">
-        <button id="btn-oui">Oui</button>
-        <button id="btn-non">Non</button>
-    </div>
+        <button id="btn-vert">Oui</button>  <button id="btn-rouge">Non</button> </div>
 
     <div id="score">Score : 0</div>
 `;
 
+// Références aux éléments HTML, avec les IDs mis à jour pour les boutons
 const imageElem = document.getElementById("question-image");
 const questionTextElem = document.getElementById("question-texte");
-const btnOui = document.getElementById("btn-oui");
-const btnNon = document.getElementById("btn-non");
+const btnVert = document.getElementById("btn-vert"); // Référence mise à jour
+const btnRouge = document.getElementById("btn-rouge"); // Référence mise à jour
 const scoreElem = document.getElementById("score");
 
 function chargerQuestion() {
@@ -89,7 +86,7 @@ function chargerQuestion() {
 function verifierReponse(reponseUtilisateur) {
     const bonneReponse = questionsNiveau1[index].reponse;
     const effet = reponseUtilisateur === bonneReponse ? "bonne-reponse" : "mauvaise-reponse";
-    const son = reponseUtilisateur === bonneReponse ? sonBon : sonMauvais;
+    const son = reponseUtilisateur === bonneReponse ? sonBon : sonMauvais; // sonMauvais sera joué si la réponse est incorrecte
 
     if (reponseUtilisateur === bonneReponse) {
         score++;
@@ -97,8 +94,8 @@ function verifierReponse(reponseUtilisateur) {
     }
 
     imageElem.classList.add(effet);
-    son.currentTime = 0;
-    son.play();
+    son.currentTime = 0; // Remet le son au début
+    son.play(); // Joue le son
 
     setTimeout(() => {
         imageElem.classList.remove(effet);
@@ -107,10 +104,11 @@ function verifierReponse(reponseUtilisateur) {
     }, 1000);
 }
 
-btnOui.addEventListener("click", () => verifierReponse("oui"));
-btnNon.addEventListener("click", () => verifierReponse("non"));
+// Les écouteurs d'événements sont maintenant attachés aux nouveaux IDs
+btnVert.addEventListener("click", () => verifierReponse("oui")); // Écouteur pour le bouton "Oui" (maintenant btn-vert)
+btnRouge.addEventListener("click", () => verifierReponse("non")); // Écouteur pour le bouton "Non" (maintenant btn-rouge)
 
-// Fonction pour revenir au menu, dejà definie dans votre index.html
+// Fonction pour revenir au menu, déjà définie dans votre index.html
 function retourMenu() {
     document.getElementById("jeu").style.display = "none";
     document.getElementById("accueil").style.display = "block";
